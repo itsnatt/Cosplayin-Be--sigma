78122d55e360082eea83431e331b287f163e4b31
itsnatt <98171083+itsnatt@users.noreply.github.com>
2024-06-23 11:40:25 +0700

    Update app.js
    -Cors
    -Logger

--- src/app.js
+++ src/app.js
@@ -8,10 +8,35 @@ const pengunjungRoutes = require('./routes/pengunjung');
 const productRoutes = require('./routes/product');
 const statusRoutes = require('./routes/status');
 const pool = require('./db');
-
+const cors = require('cors');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+
+const accessLogStream = fs.createWriteStream(path.join('.', 'access.log'), { flags: 'a' });
+app.use(morgan('combined', (tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    'from', req.connection.remoteAddress
+  ].join(' ');
+}, { stream: accessLogStream }));
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(bodyParser.json());
+
+
+
+
 
 // Define route paths and associate them with route files
 app.use('/api/address', addressRoutes);
@@ -22,5 +47,5 @@ app.use('/api/pengunjung', pengunjungRoutes);
 app.use('/api/product', productRoutes);
 app.use('/api/status', statusRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 15004;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
